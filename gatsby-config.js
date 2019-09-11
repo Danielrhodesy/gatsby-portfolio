8b6eae1d541cbc33fe0db9e6bb354efc456650e9
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-09-12 08:25:29 +1200

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const siteConfig = require('./config.js');
 const postCssPlugins = require('./postcss-config.js');
 
 module.exports = {
-  pathPrefix: siteConfig.pathPrefix,
+  pathPrefix: "/gatsby-portfolio",
   siteMetadata: {
     url: siteConfig.url,
     title: siteConfig.title,
