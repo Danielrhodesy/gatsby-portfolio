1e562a2f58f55153465a0ebad3ac01f22a8eae2a
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-09-12 08:17:19 +1200

    test deploy

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -12,8 +12,18 @@ const useSiteMetadata = () => {
               bio
               photo
               contacts {
+                facebook
                 linkedin
                 github
+                twitter
+                telegram
+                instagram
+                email
+                rss
+                vkontakte
+                line
+                gitlab
+                weibo
               }
             }
             menu {
